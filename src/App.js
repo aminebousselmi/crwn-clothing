6e6dda0d264bb1c387ddf96320e2e1515ce908b1
System Administrator <root@MacBook-Pro-de-Bousselmi.local>
2019-08-14 22:45:01 +0200

    Updating our app with a shop page, collecting preview component and collecting item component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {Route,Switch} from 'react-router-dom';
 
 import './App.css';
 import  Homepage from './pages/homepage/Homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
