20124ed3aab51d8dade62473f7423145f6c9b5f8
System Administrator <root@MacBook-Pro-de-Bousselmi.local>
2019-08-15 02:35:38 +0200

    Create Sign in pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import {Route,Switch} from 'react-router-dom';
 import './App.css';
 import  Homepage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component= {SignInAndSignUpPage} />
       </Switch>
     </div>
   );
