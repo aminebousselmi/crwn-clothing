bccf2d9dae33f0e7f9e8500d1bbd6bb0b60ddd74
System Administrator <root@MacBook-Pro-de-Bousselmi.local>
2019-08-18 22:20:56 +0200

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with redux flox directory/shop

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,19 @@
+import { createSelector } from "reselect";
+
+const SelectShop = state => state.shop;
+
+export const SelectCollections = createSelector(
+    [SelectShop],
+    shop => shop.collections
+)
+
+export const selectCollectionsForPreview = createSelector(
+    [SelectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => 
+    createSelector(
+        [SelectCollections],
+        collections => collections[collectionUrlParam]
+    )
